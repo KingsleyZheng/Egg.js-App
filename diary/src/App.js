26924b1244dd810a4342c45dbe66e0d01b300617
zhengchengzhi <zhengchengzhi@qq.com>
2020-03-14 17:19:45 +0800

    init react

---
+++ diary/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd-mobile'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type='primary'>测试</Button>
+    </div>
+  );
+}
+
+export default App;
